83e63daa10d454509852dc4bb4d811290939d569
Grant Pearce <grant.pearce@cazoo.co.uk>
2023-01-31 06:19:30 +0000

    Add stubs

--- src/PackageGeneratorServiceProvider.php
+++ src/PackageGeneratorServiceProvider.php
@@ -14,6 +14,7 @@ class PackageGeneratorServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([__DIR__ . '/Config/package_generator.php' => config_path('package_generator.php')], 'config');
+        $this->publishes([__DIR__ . '/Resources/stubs' => base_path('/resources/assets/stubs') ]);
     }
 
     /**
